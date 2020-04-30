bfc86e8f5f2f1373c1829baf54fd49f93144df97
Pavlo Velykyi <velykyi305@gmail.com>
2020-04-30 09:20:36 +0300

    hoc + context

---
+++ src/components/sw-components/person-details.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {withSwapiService} from '../hoc-helpers';
+import ItemDetails, {Record} from "../item-details";
+
+const PersonDetails = ({itemId, swapiService}) => {
+  const {getPerson, getPersonImage} = swapiService;
+  return (
+      <ItemDetails itemId={itemId}
+                   getData={getPerson}
+                   getImageUrl={getPersonImage}>
+
+        <Record field="gender" label="Gender"/>
+        <Record field="eyeColor" label="Eye color"/>
+
+      </ItemDetails>
+  );
+};
+
+export default withSwapiService(PersonDetails);

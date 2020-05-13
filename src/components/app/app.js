ea97608f65fc6e4ad64cdc67c246829ea2eaa69b
Pavlo Velykyi <velykyi305@gmail.com>
2020-05-13 08:56:27 +0300

    added redicrect, simple login, switch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,21 +5,38 @@ import RandomPlanet from "../ramdom-palanet";
 
 import "./app.css"
 import "./"
-import ErrorIndicator from "../error-indicator";
 import SwapiService from "../../services/swapi-service";
 import {SwapiServiceProvider} from "../swapi-service-context/swapi-service-context";
 import ErrorBoundary from "../error-boundary/error-boundary";
 import DummySwapiService from "../../services/dummy-swapi-service";
-import {PeoplePage, PlanetPage, StarshipPage} from "../pages";
+import {
+  LoginPage,
+  PeoplePage,
+  PlanetPage,
+  SecretPage,
+  StarshipPage
+} from "../pages";
 
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
 import StarshipDetails from "../sw-components/starship-details";
 
 export default class App extends Component {
 
   state = {
     hasError: false,
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  }
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
   }
 
   componentDidCatch(error, errorInfo) {
@@ -40,30 +57,37 @@ export default class App extends Component {
   }
 
   render() {
-
-    if (this.state.hasError) {
-      return <ErrorIndicator/>;
-    }
-
+    const {swapiService, isLoggedIn} = this.state;
     return (
         <ErrorBoundary>
-          <SwapiServiceProvider value={this.state.swapiService}>
+          <SwapiServiceProvider value={swapiService}>
             <Router>
               <div className="">
                 <Header onServiceChange={this.onChangeService}/>
                 <RandomPlanet/>
-
-                <Route path='/'
-                       render={() => <h2>Welcome to StarDB</h2>}
-                       exact/>
-                <Route path="/people/:id?" component={PeoplePage}/>
-                <Route path="/planets" component={PlanetPage}/>
-                <Route path="/starships" component={StarshipPage} exact/>
-                <Route path="/starships/:id"
-                       render={({match, location, history}) => {
-                         const {id} = match.params
-                         return <StarshipDetails itemId={id}/>
-                       }}/>
+                <Switch>
+                  <Route path='/'
+                         render={() => <h2>Welcome to StarDB</h2>}
+                         exact/>
+                  <Route path="/people/:id?" component={PeoplePage}/>
+                  <Route path="/planets" component={PlanetPage}/>
+                  <Route path="/starships" component={StarshipPage} exact/>
+                  <Route path="/starships/:id"
+                         render={({match, location, history}) => {
+                           const {id} = match.params
+                           return <StarshipDetails itemId={id}/>
+                         }}/>
+                  <Route path="/login"
+                         render={() => (
+                             <LoginPage isLoggedIn={isLoggedIn}
+                                        onLogin={this.onLogin}/>
+                         )}/>
+                  <Route path="/secret"
+                         render={() => (
+                             <SecretPage isLoggedIn={isLoggedIn}/>
+                         )}/>
+                  <Route render={() => (<h2>PageNotFound</h2>)}/>
+                </Switch>
               </div>
             </Router>
           </SwapiServiceProvider>
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,9 +1,13 @@
 import PeoplePage from "./people-page";
 import StarshipPage from "./starship-page";
 import PlanetPage from "./planet-page";
+import LoginPage from "./login-page";
+import SecretPage from "./secret-page";
 
 export {
   PeoplePage,
   PlanetPage,
-  StarshipPage
+  StarshipPage,
+  LoginPage,
+  SecretPage
 }
